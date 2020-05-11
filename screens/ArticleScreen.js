7903a46b365776c5729e68a7082b346a35371ce3
NanamiTanida <nanami_tanida@rebuild-inc.co.jp>
2020-05-11 23:25:59 +0900

    [add] ArticleScreen 追加

---
+++ screens/ArticleScreen.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, View, FlatList, SafeAreaView } from 'react-native';
+import Text from "react-native-web/dist/exports/Text"
+
+export default function ArticleScreen() {
+  return (
+    <SafeAreaView>
+      <Text>article</Text>
+    </SafeAreaView>
+  );
+};
