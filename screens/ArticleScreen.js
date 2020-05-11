02e96cf6770dbb9c0254deb1729ca524afad327c
NanamiTanida <nanami_tanida@rebuild-inc.co.jp>
2020-05-12 01:16:26 +0900

    [update] デバッガー動いた！！

--- screens/ArticleScreen.js
+++ screens/ArticleScreen.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import {SafeAreaView, StyleSheet, Text} from 'react-native'
+import {SafeAreaView, StyleSheet, TouchableOpacity, Text} from 'react-native'
 import { WebView } from 'react-native-webview';
+import { useDispatch } from 'react-redux';
+import { addClip, deleteClip } from '../store/actions/user';
 
 const styles = StyleSheet.create({
   container: {
@@ -11,9 +13,16 @@ const styles = StyleSheet.create({
 
 export default function ArticleScreen({route}) {
   const {article} = route.params;
+  const dispatch = useDispatch();
   console.log(article);
   return (
     <SafeAreaView style={styles.container}>
+      <TouchableOpacity onPress={() => { dispatch(addClip({clip: article})) }}>
+        <Text style={{ margin: 10, fontSize: 30 }}>Add Clip</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => { dispatch(deleteClip({ clip: article })) }}>
+        <Text style={{margin: 10, fontSize: 30}}>DELETE_CLIP</Text>
+      </TouchableOpacity>
       <WebView source={{uri: article.url}} />
     </SafeAreaView>
   );
--- store/index.js
+++ store/index.js
@@ -2,9 +2,9 @@ import { createStore, combineReducers } from 'redux';
 import userReducer from './reducers/user';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
-const rootReducer = combineReducers ({
-  user: userReducer
-})
+const rootReducer = combineReducers({
+  user: userReducer,
+});
 
 const store = createStore(rootReducer, composeWithDevTools());
 
