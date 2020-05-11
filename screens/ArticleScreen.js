bab2338a635e8d1237f6704554a287757a62878f
NanamiTanida <nanami_tanida@rebuild-inc.co.jp>
2020-05-11 23:36:36 +0900

    [update] webviewでurlで遷移まで確認

--- screens/ArticleScreen.js
+++ screens/ArticleScreen.js
@@ -1,10 +1,18 @@
 import React from 'react';
-import { SafeAreaView, Text } from 'react-native';
+import {SafeAreaView, StyleSheet, Text} from 'react-native'
+import { WebView } from 'react-native-webview';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+});
 
 export default function ArticleScreen() {
   return (
-    <SafeAreaView>
-      <Text>article</Text>
+    <SafeAreaView style={styles.container}>
+      <WebView source={{uri: 'https://expo.io'}} />
     </SafeAreaView>
   );
 };
